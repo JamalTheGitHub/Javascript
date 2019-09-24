a57976debd0fd8ea35f8fc62d1c515abacc11329
JamalTheBucket <muhammadeezzuan@mustardworldwide.com>
2019-09-24 14:40:19 +0800

    fixed to proper syntax in script.js file

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 // This is a comment. We can use this to write in the .js extension without worrying if it might affect the code.
 // linked to index.html
 
-alert("This is the console section!")
+console.log("This is the console section!")
 alert("Hello World!");
